ecf40c21b0042b5e5808d311a5dd3f42c7daa42d
Eric Cleveland <eric@clevelandcloud.com>
2019-07-03 11:44:26 -0500

    Day 1: At the office

---
+++ Program_Flow/1.1_MultithreadingAndAsync/Parallel/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Linq;
+
+namespace Example
+{
+    class Program
+    {
+        static void Task1()
+        {
+            Console.WriteLine("Task 1 Starting");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task 1 Ending");
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine("Task 2 Starting");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task 2 Ending");
+        }
+
+        static void WorkOnItem(object item)
+        {
+            Console.WriteLine($"Started processing {item}");
+            Thread.Sleep(100);
+            Console.WriteLine($"Finished working on {item}");
+        }
+
+        static void Main(string[] args)
+        {
+            // Parallel.Invoke can start a large number of tasks but you have no control over the order in 
+            // which the tasks are started or which processor they are assigned to
+            Console.WriteLine("Side by side 'Invoke' Processing:");
+            Parallel.Invoke(() => Task1(), () => Task2());
+
+            // Parallel.ForEach
+            var itemsForEach = Enumerable.Range(0, 500);
+            Parallel.ForEach(itemsForEach, item =>
+            {
+                WorkOnItem(item);
+            });
+
+            // Managing a parallel For Loop
+            // Calling stop prevents any new iterations with an index value greater than the current index.
+            var itemsFor = Enumerable.Range(0, 500).ToArray();
+            ParallelLoopResult result = Parallel.For(0, itemsFor.Count(), (int i, ParallelLoopState loopState) => {
+                if(i == 200)
+                    loopState.Stop();
+
+                WorkOnItem(itemsFor[i]);
+            });
+            Console.WriteLine($"Completed { result.IsCompleted }");
+            Console.WriteLine($"Items: " + result.LowestBreakIteration);
+
+            Console.WriteLine("Press any key to end.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program_Flow/1.1_MultithreadingAndAsync/ParallelLinq/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace ParallelLinq
+{
+    class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+            public string City { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[] {
+                new Person { Name = "Eric Cleveland", City = "Frisco" },
+                new Person { Name = "Jesus Christ", City = "Nazareth" },
+                new Person { Name = "Abbie Schwartz", City = "Ziopupo" },
+                new Person { Name = "Todd Hunter", City = "Tetiswi" },
+                new Person { Name = "Katharine Perkins", City = "Dengupom" },
+                new Person { Name = "Amy Miller", City = "Nuzejo" },
+                new Person { Name = "Ann Boyd", City = "Dengupom" },
+                new Person { Name = "Clara Henderson", City = "Dengupom" },
+                new Person { Name = "Harvey Santos", City = "Gatjukbas" },
+                new Person { Name = "Iva Baldwin", City = "Rumgijed" },
+                new Person { Name = "Ella Bryant", City = "Ekzavud" }
+            };
+// TODO: Return to listing 1-6 informing parallelization (pdf:32)
+            var result = from person in people.AsParallel()
+                         where person.City == "Dengupom"
+                         select person;
+            
+            foreach (var person in result)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            Console.WriteLine("Finished processing. Press a key to end.");
+            Console.ReadKey();
+        }
+    }
+}
